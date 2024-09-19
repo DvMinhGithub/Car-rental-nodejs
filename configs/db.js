654f76cb4fea12c2cfb4c60ce52953230ee24c52
DvMinhGithub <minhdv.dev@gmail.com>
2024-09-19 08:33:09 +0700

    feat: Add MySQL database configuration and connection

---
+++ configs/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+console.log(connection);
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
+const connection = require('./configs/db');
+const morgan = require('morgan');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  connection.query('SELECT * FROM category', (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
 });
 
 app.listen(PORT, () => {
